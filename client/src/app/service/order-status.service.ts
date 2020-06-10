b9bed2bfb65ae3add99fae36ceaad8906213ad9a
Arisa Rangklang <56140694+arifirst@users.noreply.github.com>
2020-06-11 02:45:49 +0700

    Add order status UI - close #2

---
+++ client/src/app/service/order-status.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const baseUrl = 'http://localhost:3000/api/addlist';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderStatusService {
+
+  t_id: number
+  t_name: string
+
+  status_type: any = [
+    {t_id: 1,t_name:"ที่ต้องชำระ"},
+    {t_id: 2,t_name:"กำลังจัดส่ง"},
+    {t_id: 3,t_name:"ที่ต้องได้รับ"},
+    {t_id: 4,t_name:"เสร็จสิ้น"}
+  ]
+
+  constructor(private http: HttpClient) { }
+
+  getAllStatusType(){
+    return this.status_type;
+  }
+}
